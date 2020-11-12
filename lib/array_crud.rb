f57d2a7dea58b51d94e0c1185024e093ca0b58ba
Jeff Golden <gjgolden08@gmail.com>
2020-11-12 00:39:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,26 @@
+require 'pry'
+
 def create_an_empty_array
-  
+  []
+
 end
 
 def create_an_array
+  ["dog","cat","hamster","horse"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << "arrays!"
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
+  array.unshift["wow"]
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -27,11 +28,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
